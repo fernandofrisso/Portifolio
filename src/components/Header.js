e40d0cf8b0a721e92010a63bd5d0ac6c2bc8afcb
fernando frisso <fernandofrisso@outlook.com>
2023-05-05 11:24:33 -0300

    Created Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import Logo from '../assets/logo9.png'
+
 const Header = () => {
-  return <div className='bg-pink-300'>Header</div>;
+
+  return <header className='py-8'>
+
+    <div className='container mx-auto'>
+
+        <div className='flex justify-between items-center'>
+          
+            {/*logo*/}
+
+            <a href='#'>
+
+              <img src={Logo} alt='' width='170px'/>
+
+            </a>
+
+            <button className='btn btn-sm'>Work with me</button>
+
+        </div>
+
+    </div>
+
+
+  </header>;
 };
 
 export default Header;
