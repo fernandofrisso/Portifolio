23db5f8ead2aafe6b8edd096c36bee4818a86e33
fernando frisso <fernandofrisso@outlook.com>
2023-05-08 18:20:02 -0300

    upgrade banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -29,13 +29,25 @@ const Banner = () => {
           {/*text*/}
           <div className='flex-1 text-center font-secondary lg:text-left'>
 
-            <h1 className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+            <motion.h1 
+
+            variants={fadeIn('up', 0.3)} 
+            initial='hidden' 
+            whileInView={'show'} 
+            viewport={{once: false, amount: 0.7}} 
+            className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
               
               FERNANDO <span>FRISSO</span>
 
-            </h1>
+            </motion.h1>
 
-            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading -[1]'>
+            <motion.div 
+
+            variants={fadeIn('up', 0.4)} 
+            initial='hidden' 
+            whileInView={'show'} 
+            viewport={{once: false, amount: 0.7}} 
+            className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading -[1]'>
 
               <span className=' text-white mr-4'>I am a</span>
 
@@ -55,19 +67,41 @@ const Banner = () => {
 
               />
 
-            </div>
+            </motion.div>
 
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet magna quis tellus vehicula maximus. Quisque vitae lectus sed leo dictum dictum at at ipsum.</p>
+            <motion.p 
+
+            variants={fadeIn('up', 0.5)} 
+            initial='hidden' 
+            whileInView={'show'} 
+            viewport={{once: false, amount: 0.7}} 
+            className='mb-8 max-w-lg mx-auto lg:mx-0'>
+              
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet magna quis tellus vehicula maximus. Quisque vitae lectus sed leo dictum dictum at at ipsum.
             
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            </motion.p>
+            
+            <motion.div 
+
+            variants={fadeIn('up', 0.6)} 
+            initial='hidden' 
+            whileInView={'show'} 
+            viewport={{once: false, amount: 0.7}} 
+            className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
 
                 <button className='btn btn-lg'>Contact me</button>
                 < a href='#' className='text-gradient btn-link'> My Portifólio</a>
 
-            </div>
+            </motion.div>
 
-            {/*social*/}
-            <div className='flex text-[20] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            {/*socials*/}
+            <motion.div 
+
+            variants={fadeIn('up', 0.7)} 
+            initial='hidden' 
+            whileInView={'show'} 
+            viewport={{once: false, amount: 0.7}} 
+            className='flex text-[20] gap-x-6 max-w-max mx-auto lg:mx-0'>
 
               <a href='#'>
                 <FaGithub/>
@@ -77,15 +111,21 @@ const Banner = () => {
                 <FaLinkedin/>
               </a>
 
-            </div>
+            </motion.div>
 
           </div>
 
           {/*image*/}
 
-          <div>
+          <motion.div 
+          
+            variants={fadeIn('down', 0.5)} 
+            initial='hidden' 
+            whileInView={'show'} 
+            viewport={{once: false, amount: 0.7}} 
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto'>
             <img src={image} alt=''/>
-          </div>
+          </motion.div>
 
         </div>
 
